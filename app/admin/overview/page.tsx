981632b5f23f97c7036b0010797cfbd2edd28e70
Dragon2792 <muhammadaryanurulfata@gmail.com>
2026-03-18 14:48:54 +0700

    gulir1

---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,22 @@
+<div className={styles.tableCard}>
+  <div className={styles.tableWrapper}>
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <th>Field</th>
+          <th>Value</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Field 1</td>
+          <td>Value 1</td>
+        </tr>
+        <tr>
+          <td>Field 2</td>
+          <td>Value 2</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>;
