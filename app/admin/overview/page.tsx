531d291bb7845de3f97c1ab4519d27814180c578
Dragon2792 <muhammadaryanurulfata@gmail.com>
2026-03-18 14:52:00 +0700

    perbaikan

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -1,22 +1,28 @@
-<div className={styles.tableCard}>
-  <div className={styles.tableWrapper}>
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>Field</th>
-          <th>Value</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Field 1</td>
-          <td>Value 1</td>
-        </tr>
-        <tr>
-          <td>Field 2</td>
-          <td>Value 2</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>;
+import styles from "./overview.module.css";
+
+export default function AdminOverview() {
+  return (
+    <div className={styles.tableCard}>
+      <div className={styles.tableWrapper}>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Field</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Field 1</td>
+              <td>Value 1</td>
+            </tr>
+            <tr>
+              <td>Field 2</td>
+              <td>Value 2</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
